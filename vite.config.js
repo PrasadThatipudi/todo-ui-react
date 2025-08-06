e2d1ab19233ad854ffaafabb7f6d0b690c805a60
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-08-06 07:58:26 +0530

    Updates backend server URL

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/todo-api": {
-        target: "http://localhost:8000",
+        target: "https://todo-app-jvru.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/todo-api/, ""),
       },
